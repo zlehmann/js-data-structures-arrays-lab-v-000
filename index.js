010696ae516aa4aa7ed09c6bfb9b98991ad64686
Zachary Lehmann <zlehmann@gmail.com>
2019-03-10 19:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
+
+function appendDriver(name) {
+  append_drivers = [...drivers, name];
+}
